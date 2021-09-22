456894fe07a1adcef5602382d811fc7396058f10
Natalia CAmpiglia <campiglianatalia@gmail.com>
2021-09-21 22:35:42 -0300

    New images

--- src/app/types/constants.ts
+++ src/app/types/constants.ts
@@ -7,45 +7,19 @@ export const MENU = {
   team: 'equipo',
 };
 
-export const IMAGES = [
-  'IMG_4009.png',
-  'IMG_4028.png',
-  'IMG_4029.png',
-  'IMG_4030.png',
-
-  'IMG_4031.png',
-  'IMG_4044.png',
-  'IMG_4033.png',
-
-  'IMG_4034.png',
-  'IMG_4035.png',
-  'IMG_4036.png',
-
-  'IMG_4037.png',
-  'IMG_4038.png',
-  'IMG_4039.png',
-
-  'IMG_4040.png',
-
-  'IMG_4020.png',
-  'IMG_4021.png',
-
-  'IMG_4045.png',
-  'IMG_4046.png',
-  'IMG_4048.png',
-];
+export const IMAGES = ['image-1.png', 'image-2.png', 'image-3.png', 'image-4.png', 'image-5.png', 'image-6.png', 'image-7.png'];
 
 export const DOCTORS_TEAM = [
-  {name: 'Mariana Bonifacino', description: 'Doctora, especialista en Alergología', image: 'mariana-bonifacino.png'},
-  {name: 'Rosario Eugui', description: 'Doctora, especialista en Otorrinolaringología', image: 'rosario-eugui.png'},
+  {name: 'Carina Almirón', description: 'Doctora, especialista en Otorrinolaringología', image: 'carina-almiron.png'},
+  {name: 'Mariana Bonifacino', description: 'Doctora, Postgrado de Alergología', image: 'mariana-bonifacino.png'},
   {name: 'Patricia Indarte', description: 'Doctora, especialista en Otorrinolaringología', image: 'patricia-indarte.png'},
-  {name: 'Carina Almirón', description: 'Doctora, especialista en Otorrinolaringología', image: 'doctor.svg'},
+  {name: 'Rosario Eugui', description: 'Doctora, especialista en Otorrinolaringología', image: 'rosario-eugui.png'},
 ];
 
 export const BIOLOGY_TEAM = [
   {name: 'Ángeles Beri', description: 'Doctora en Ciencias Biológicas y responsable del proyecto', image: 'angeles-beri.png'},
-  {name: 'Ximena Martínez', description: 'Doctora en Ciencias Biológicas Otorrinolaringología.', image: 'scientist.svg'},
   {name: 'Alejandra Leal', description: 'Doctorado en Ciencias Biológicas', image: 'scientist.svg'},
+  {name: 'Ximena Martínez', description: 'Doctora en Ciencias Biológicas Otorrinolaringología.', image: 'scientist.svg'},
 ];
 
 export const ENGINEER_TEAM = [
